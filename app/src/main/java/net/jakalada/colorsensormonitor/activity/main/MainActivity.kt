2d04eb62a87ba8dd57bdf444aca6a5a4f8f6978d
Hideki Hamada (jakalada) <jakalada.net@gmail.com>
2018-07-16 22:23:46 +0900

    MainActivityのパッケージを変更

--- app/src/main/java/net/jakalada/colorsensormonitor/activity/main/MainActivity.kt
+++ app/src/main/java/net/jakalada/colorsensormonitor/activity/main/MainActivity.kt
@@ -1,13 +1,22 @@
 package net.jakalada.colorsensormonitor.activity.main
 
+import android.content.Intent
+import android.hardware.Sensor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import net.jakalada.colorsensormonitor.R
+import net.jakalada.colorsensormonitor.activity.sensorlist.SensorListActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorListButton.setOnClickListener {
+            val intent = Intent(this, SensorListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/net/jakalada/colorsensormonitor/activity/sensorlist/SensorListActivity.kt
@@ -0,0 +1,13 @@
+package net.jakalada.colorsensormonitor.activity.sensorlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.jakalada.colorsensormonitor.R
+
+class SensorListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensor_list)
+    }
+}
